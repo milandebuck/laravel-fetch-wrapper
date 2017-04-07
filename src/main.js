47c2f8d5dc9374baf736ffb95dc448087c0bdb3c
Milan De Buck <milan.de.buck@telenet.be>
2017-04-07 15:17:09 +0100

    added posibility to define global headers who are applied to each request

--- src/main.js
+++ src/main.js
@@ -7,17 +7,30 @@
 */
 module.exports ={
 
+	//sets a permanent header value used for all requests
+	setHeader(name,value){
+		if(typeof name !== 'string')throw new Error('name of the header should be a String not an ' + typeof name)
+		if(!this.headers)this.headers=[];
+		this.headers.push({name:name,value:value});
+	},
+
+	//remove a Header from the permanent header list
+	removeHeader(name){
+
+	},
 	//middleware to apply the laravel header to the fetch api headers
 	_applyHeaders(headers){
 		if(!this.csrfToken) throw new Error('No CSRF Token provided use the setToken method first')
 		headers.append('X-CSRF-TOKEN',this.csrfToken)
 		headers.append('X-Requested-With','XMLHttpRequest')
+		if(this.headers)this.headers.forEach(h => headers.append(h.name,h.value));
 		return headers
 	},
 
 	//set a token
 	setToken(token){
 		if(!token || token ===  undefined || token.length === 0)throw new Error('Invalid token');
+		if(typeof token !== 'string')throw new Error('Token schould be a string not an' + typeof token)
 		this.csrfToken=token;
 	},
 
